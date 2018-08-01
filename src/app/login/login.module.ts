7fdd6addf32fc27d6536f13e9d4ea2992ca58862
Lakjeewa <lakjeewa@Populos-Mac-mini-2.local>
2018-08-01 14:28:54 +0530

    created post request to login user and added routes

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [RegisterComponent]
+  declarations: [LoginComponent]
 })
-export class RegisterModule { }
+export class LoginModule { }
